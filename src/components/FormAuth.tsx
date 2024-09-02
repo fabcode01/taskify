6c988fc1a70e5d43c999f4f0c252b83af6b20af8
Fabrício Silva <fabricio85silva@hotmail.com>
2024-09-02 18:01:45 -0300

    add: propriedades de componentes faltando

--- src/components/FormAuth.tsx
+++ src/components/FormAuth.tsx
@@ -3,6 +3,7 @@
 import { airplaneIcon, googleIcon } from "./icons"
 import Input from "./Input"
 import Button from "./Button"
+import { useState } from "react"
 
 interface FormProps{
     title: string
@@ -11,6 +12,18 @@ interface FormProps{
 }
 
 export function Form(props: FormProps){
+    const[email, setEmail] = useState('')
+    const[password, setPassword] = useState('')
+
+    function loginWithEmail(){
+        console.log(email, password);
+        
+    }
+
+    function loginWithGoogle(){
+        console.log(email, password);
+    }
+
 
     return (
         <div>
@@ -18,12 +31,12 @@ export function Form(props: FormProps){
 
             <div className="flex flex-col items-center">
 
-                <Input inputType="text" placeholder="yourEmail@exemple.com"/>
-                <Input inputType="password" placeholder="Password"/>
+                <Input inputType="text" placeholder="yourEmail@exemple.com" onChange={setEmail}/>
+                <Input inputType="password" placeholder="Password" onChange={setPassword}/>
 
                 <div className="flex gap-2">
-                    <Button icon={googleIcon} className="bg-gray-300"/>
-                    <Button icon={airplaneIcon} className="bg-azul-escuro text-azul-clarinho hover:bg-blue-950"/>
+                    <Button icon={googleIcon} className="bg-gray-300" submit={loginWithGoogle}/>
+                    <Button icon={airplaneIcon} className="bg-azul-escuro text-azul-clarinho hover:bg-blue-950" submit={loginWithEmail}/>
                 </div>
 
 
