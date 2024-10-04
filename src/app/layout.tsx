6e65de044dba67985820cf61602b3b7f6e37b9ae
fabcode01 <fabricio85silva@hotmail.com>
2024-10-04 11:25:40 -0300

    add: meta / metatags para mudar cor do navegador

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,12 @@
 
 import type { Metadata } from "next";
 import "./globals.css";
-import { AuthProvider } from "@/context/AuthContext";
 
 
 export const metadata: Metadata = {
   title: "Taskify",
   description: "Taskify. Gerenciador de tarefas",
+  
   
 };
 
@@ -20,6 +20,12 @@ export default function RootLayout({
 
       <html lang="pt-br" className="bg-white">
         <head>
+          <meta name="theme-color" content="#021E2B" />
+          <meta name="msapplication-navbutton-color" content="#4285f4" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+
+
           <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
         </head>
           <body>{children}</body>
